774ce2f1264f86a5bc4c5478fb831f48d43de0ff
John Brumbaugh <john.brumbaugh@gmail.com>
2016-02-29 17:26:46 -0500

    Updated to create new grunt task for deployment assuming same illiniboard/ root directory for all illiniboard projects, specifically illiniboard-frontend and illiniboard-static.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-copy');
     require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
     grunt.initConfig({
         sass: {
@@ -7,8 +8,20 @@ module.exports = function(grunt) {
                     'dist/illiniboard.css' : 'assets/foundation.scss'
                 }
             }
+        },
+        copy: {
+            default_development_deploy: {
+                files: [
+                    {
+                        
+                        src: ['dist/illiniboard.css'], 
+                        dest: '../illiniboard-frontend/static/css/illiniboard.css', 
+                        filter: 'isFile'}
+                ]
+            }
         }
     });
 
-    grunt.registerTask('default', ['sass']);   
+    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('dev_deploy', ['copy:default_development_deploy']);
 }
